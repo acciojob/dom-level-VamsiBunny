4b3d91138f0110d9a36c79cf45c13405c3b22b4a
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-16 11:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let level=document.getElementByID("level");
+
+let domLevel=1;
+
+while(level.parentElement!==null){
+	level=level.parentElement;
+	domLevel++
+}
+
+console.log("The level of the element is: ",domLevel);
